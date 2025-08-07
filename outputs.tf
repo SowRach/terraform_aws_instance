3da82be8625d8659f65c37fd867431600d69a940
Nuthan <Nuthan.gatla@gmail.com>
2025-08-07 18:38:47 +0530

    initial commit

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "private_ip" {
   description = "The private IP of the EC2 instance"
   value       = aws_instance.web.private_ip
 }
+
+
+output "id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
